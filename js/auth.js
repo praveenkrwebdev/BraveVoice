860ec095849f3a87fd4fe5ee9e86c5983cb019c3
praveenkrwebdev <praveenkr.webdev@gmail.com>
2026-01-01 23:50:35 +0530

    Enable authentication state redirection

--- js/auth.js
+++ js/auth.js
@@ -79,10 +79,11 @@ window.logout = () => {
 };
 
 /* AUTH STATE (OPTIONAL BUT BEST PRACTICE) */
-// onAuthStateChanged(auth, user => {
-//   if (!user && !location.pathname.includes("login")) {
-//     window.location.href = "login.html";
-//   }
+onAuthStateChanged(auth, user => {
+  if (!user && !location.pathname.includes("login")) {
+    window.location.href = "login.html";
+  
+
     /* 👁️ TOGGLE PASSWORD VISIBILITY */
 window.togglePassword = () => {
   const passwordInput = document.getElementById("password");
